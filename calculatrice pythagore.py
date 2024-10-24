67ab22193bbd2910e0e267d7073d9183d3caba17
karlweidig <168371286+karlweidig@users.noreply.github.com>
2024-10-24 17:09:01 +0200

    Calculateur Pythagore
    Voici un calculateur qui calcule la formule de Pythagore.

---
+++ calculatrice pythagore.py
@@ -0,0 +1,16 @@
+import math
+
+print("Voici un calculateur Python!")
+reponse_choix = input("Que voulez-vous connaitre !(hypothènuse [h] ou  cathète [c])")
+a = int(input("Quelle est la longueur du premier cathètes ?"))
+if reponse_choix.lower() == "h":
+    b = int(input("Quelle est la longueur du deuxième cathètes ?"))
+    x = a**2 + b**2
+    x = math.sqrt(x)
+    print(f"L'hypothènuse vaut: {x}")
+
+if reponse_choix.lower() == "c":
+    b = int(input("Quelle est la longueur de l'hypothènuse ?"))
+    x = b**2 - a**2
+    x = math.sqrt(x)
+    print(f"Le deuxième cathète vaut: {x}")
